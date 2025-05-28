66aa6af07f138d8b9e741c27c2bd82994efb7e02
Goydaa <URNAZXC@GMAIL.COM>
2025-05-28 21:18:55 +0300

    Update CinemaService.cs

--- CinemaService.cs
+++ CinemaService.cs
@@ -1,66 +1,31 @@
-
-using System;
-using System.Collections.Generic;
-
 public class CinemaService
 {
-    private Database _db;
-
-    public CinemaService(Database db)
+    private List<string> movies = new List<string>
     {
-        _db = db;
-    }
+        "The Matrix",
+        "Inception",
+        "Interstellar"
+    };
 
-    // Получение списка фильмов
-    public List<string> ListAvailableMovies()
+    public void ShowAllMovies()
     {
-        return _db.GetMovies();
-    }
-
-    // Получение сеансов по ID фильма
-    public List<string> GetScreeningsForMovie(int movieId)
-    {
-        return _db.GetScreeningsByMovie(movieId);
-    }
-
-    // Получение свободных мест (заглушка)
-    public List<string> GetFreeSeats(int screeningId)
-    {
-        // Тут должна быть логика получения занятых мест из БД
-        return new List<string> { "A1", "A2", "A3", "B1", "B2" };
-    }
-
-    // Бронирование билета с проверкой возраста
-    public bool TryBookTicket(int screeningId, int userId, string seatNumber, int userAge, int ageLimit)
-    {
-        if (userAge < ageLimit)
+        Console.WriteLine("\nДоступные фильмы:");
+        foreach (var movie in movies)
         {
-            Console.WriteLine("Ошибка: возраст пользователя ниже возрастного ограничения фильма.");
-            return false;
-        }
-
-        try
-        {
-            _db.BookTicket(screeningId, userId, seatNumber, ageLimit);
-            return true;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Ошибка при бронировании билета: " + ex.Message);
-            return false;
+            Console.WriteLine($"- {movie}");
         }
     }
 
-    // Получение списка бронирований пользователя (заглушка)
-    public List<string> GetUserBookings(int userId)
+    public void BookTicket(string movieName)
     {
-        return new List<string> { $"Бронь: Сеанс 1, Место A1", $"Бронь: Сеанс 2, Место B2" };
-    }
-
-    // Отмена бронирования (заглушка)
-    public bool CancelBooking(int bookingId, int userId)
-    {
-        Console.WriteLine($"Бронирование {bookingId} отменено.");
-        return true;
+        if (movies.Contains(movieName))
+        {
+            Console.WriteLine($"Билет забронирован: {movieName}");
+        }
+        else
+        {
+            Console.WriteLine("Фильм не найден.");
+        }
     }
 }
+
