0da0e2581af0c9ec1475466253a3dbcd596aa130
Goydaa <URNAZXC@GMAIL.COM>
2025-05-28 11:04:11 +0300

    Update CinemaService.cs

--- CinemaService.cs
+++ CinemaService.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 
@@ -10,16 +11,26 @@ public class CinemaService
         _db = db;
     }
 
+    // Получение списка фильмов
     public List<string> ListAvailableMovies()
     {
         return _db.GetMovies();
     }
 
+    // Получение сеансов по ID фильма
     public List<string> GetScreeningsForMovie(int movieId)
     {
         return _db.GetScreeningsByMovie(movieId);
     }
 
+    // Получение свободных мест (заглушка)
+    public List<string> GetFreeSeats(int screeningId)
+    {
+        // Тут должна быть логика получения занятых мест из БД
+        return new List<string> { "A1", "A2", "A3", "B1", "B2" };
+    }
+
+    // Бронирование билета с проверкой возраста
     public bool TryBookTicket(int screeningId, int userId, string seatNumber, int userAge, int ageLimit)
     {
         if (userAge < ageLimit)
@@ -38,5 +49,18 @@ public class CinemaService
             Console.WriteLine("Ошибка при бронировании билета: " + ex.Message);
             return false;
         }
+    }
+
+    // Получение списка бронирований пользователя (заглушка)
+    public List<string> GetUserBookings(int userId)
+    {
+        return new List<string> { $"Бронь: Сеанс 1, Место A1", $"Бронь: Сеанс 2, Место B2" };
+    }
+
+    // Отмена бронирования (заглушка)
+    public bool CancelBooking(int bookingId, int userId)
+    {
+        Console.WriteLine($"Бронирование {bookingId} отменено.");
+        return true;
     }
 }
